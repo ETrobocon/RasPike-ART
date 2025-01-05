9a51b4ebe2c0a383430f3020ce070e99c5090b02
ytoi <y_toi@mac.com>
2025-01-05 08:46:26 +0000

    Changed Button/Battery #1

--- sdk/common/library/libcpp-spike/include/Button.h
+++ sdk/common/library/libcpp-spike/include/Button.h
@@ -14,41 +14,46 @@ namespace spikeapi {
 class Button
 {
 public:
-    /**
-     * コンストラクタ
-     * @param -
-     * @return -
-     */
-    Button(void) = default;
-
-    /**
-     * LEFTボタンが押されているかを確認する
-     * @param -
-     * @return -
-     */
-   bool isLeftPressed();
+  /**
+   * コンストラクタ
+   * @param -
+   * @return -
+   */
+  Button(void) = default;
   
-    /**
-     * CENTERボタンが押されているかを確認する
-     * @param -
-     * @return -
-     */
-   bool isCenterPressed();
+  /**
+   * LEFTボタンが押されているかを確認する
+   * @param -
+   * @return -
+   */
+  bool isLeftPressed();
+  
+  /**
+   * CENTERボタンが押されているかを確認する
+   * @param -
+   * @return -
+   */
+  bool isCenterPressed();
 
-    /**
-     * RIGHTボタンが押されているかを確認する
-     * @param -
-     * @return -
-     */
-   bool isRightPressed();
+  /**
+   * RIGHTボタンが押されているかを確認する
+   * @param -
+   * @return -
+   */
+  bool isRightPressed();
 
-    /**
-     * BRUETOOTHボタンが押されているかを確認する
-     * @param -
-     * @return -
-     */
-   bool isBluetoothPressed();
+  /**
+   * BRUETOOTHボタンが押されているかを確認する
+   * @param -
+   * @return -
+   */
+  bool isBluetoothPressed();
 
+  /**
+   * インスタンス生成が正常にできたかどうかを確認するための共通メソッド
+   * Buttonでは複数生成が問題ないので、常にfalseを返す
+   */
+  bool hasError() { return false; }
   
 }; // class Button
 }  // namespace spikeapi
