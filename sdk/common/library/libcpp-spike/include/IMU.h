b0ce34c04bc244b14b442f584be2fab0081cfa1d
ytoi <y_toi@mac.com>
2025-01-05 10:56:54 +0000

    changed IMU

--- sdk/common/library/libcpp-spike/include/IMU.h
+++ sdk/common/library/libcpp-spike/include/IMU.h
@@ -7,10 +7,9 @@
 #ifndef SPIKE_CPP_API_IMU_H_
 #define SPIKE_CPP_API_IMU_H_
 
+extern "C" {
 #include <spike/hub/imu.h>
-
-#include "AccelerationDto.h"
-#include "AngularVelocityDto.h"
+}
 
 namespace spikeapi {
 /**
@@ -19,44 +18,62 @@ namespace spikeapi {
 class IMU
 {
 public:
-    /**
-     * コンストラクタ
-     * @param -
-     * @return -
-     */
-    IMU(void) = default;
+
+  /* 加速度 mm/s^2*/
+  struct Acceleration {
+    float x;
+    float y;
+    float z;
+  };
+
+  /* 角速度 degree/s */
+  struct AngularVelocity {
+    float x;
+    float y;
+    float z;
+  };
     
-    /** 
-     * IMUドライバを初期化する
-     * @param -
-     * @return -
-     */ 
-    inline void init() const { 
-        hub_imu_init(); 
-    }
+
+  /**
+   * コンストラクタ
+   * @param -
+   * @return -
+   */
+  IMU(void) {
+    // このinitは2回目以降エラーとなるが、値を取る上では問題ないためそのままとする
+    hub_imu_init();
+  }
     
-    /** 
-     * IMUから加速度を取得する
-     * @param -
-     * @return x/y/z軸の加速度を格納するためのfloat配列[mm/s^2] 
-     */ 
-    AccelerationDto getAcceleration();
+  /** 
+   * IMUから加速度を取得する
+   * @param x/y/z軸の加速度を格納するためのAcceleration構造体[mm/s^2] 
+   * @return -
+   */ 
+  void getAcceleration(Acceleration &accel);
     
-    /** 
-     * IMUから角速度を取得する
-     * @param -
-     * @return x/y/z軸の角速度を格納するためのfloat配列[°/s] 
-     */ 
-    AngularVelocityDto getAngularVelocity();
+  /** 
+   * IMUから角速度を取得する
+   * @param x/y/z軸の角速度を格納するためのAngularVelocity構造体配列[°/s] 
+   * @return -
+   */ 
+  void getAngularVelocity(AngularVelocity &ang);
     
-    /** 
-     * IMUから温度を取得する
-     * @param -
-     * @return 温度[℃] 
-     */
-    inline float getTemperature() const { 
-        return hub_imu_get_temperature(); 
-    }
+  /** 
+   * IMUから温度を取得する
+   * @param -
+   * @return 温度[℃] 
+   */
+  float getTemperature() const { 
+    return hub_imu_get_temperature(); 
+  }
+
+  /**
+   * インスタンス生成が正常にできたかどうかを確認するための共通メソッド
+   * IMUでは複数生成が問題ないので、常にfalseを返す
+   */
+  bool hasError() { return false; }
+  
+  
 }; // class IMU
 }  // namespace spikeapi
 
